c34a6a54d5ac7aa3ac6323325a065334bce13bd5
Asha Kar <Asha@Ashas-MacBook-Pro.local>
2018-04-29 17:39:24 -0700

    Zomato

--- FinalProject/scripts.js
+++ FinalProject/scripts.js
@@ -2,6 +2,12 @@
 var request = new XMLHttpRequest();
 //open new connection, using GET request on URL endpoint
 request.open('GET', 'https://ghibliapi.herokuapp.com/films', true);
+/**var url = "https://developers.zomato.com/api/v2.1/restaurant?res_id=RESID";
+request.open('GET', url, true);
+request.send();
+var received = JSON.parse(this.response);
+{Need to then access price}
+**/
 
 request.onload = function (){
 // begin accessing json data here
