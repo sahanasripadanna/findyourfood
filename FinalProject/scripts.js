d1a59e9dd144e31c7d051f783117bb36310aa522
Asha Kar <Asha@Ashas-MacBook-Pro.local>
2018-04-30 20:04:59 -0700

    updated a LOT

--- FinalProject/scripts.js
+++ FinalProject/scripts.js
@@ -1,14 +1,31 @@
-//create a request variable and assing a new XMLHttpRequest object
-var request = new XMLHttpRequest();
+
+function searched(){
+  var searchedWord = document.getElementById("searchTerm").value;
+  alert(searchedWord);
+  document.write('You searched for "' + searchedWord + '"');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 //open new connection, using GET request on URL endpoint
-request.open('GET', 'https://ghibliapi.herokuapp.com/films', true);
+
 /**var url = "https://developers.zomato.com/api/v2.1/restaurant?res_id=RESID";
 request.open('GET', url, true);
 request.send();
 var received = JSON.parse(this.response);
 {Need to then access price}
 **/
-
+/*** 
 request.onload = function (){
 // begin accessing json data here
   var data = JSON.parse(this.response);
@@ -20,4 +37,4 @@ request.onload = function (){
 }
 
 // send request
-request.send();
+request.send();**/
