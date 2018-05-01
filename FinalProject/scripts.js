cd7ce24f23b19674a786793b88386f22798aea59
Asha Kar <Asha@Ashas-MacBook-Pro.local>
2018-04-30 21:43:08 -0700

    responsive to user searches

--- FinalProject/scripts.js
+++ FinalProject/scripts.js
@@ -2,7 +2,19 @@
 function searched(){
   var searchedWord = document.getElementById("searchTerm").value;
   alert(searchedWord);
-  document.write('You searched for "' + searchedWord + '"');
+ 
+    // create a new div element 
+    var newDiv = document.createElement("div"); 
+    // and give it some content 
+    var newContent = document.createTextNode("You searched'" + searchedWord+"'"); 
+    // add the text node to the newly created div
+    newDiv.appendChild(newContent);  
+  
+    // add the newly created element and its content into the DOM 
+    var currentDiv = document.getElementById("wrap"); 
+    document.body.insertBefore(newDiv, currentDiv); 
+  
+ // document.write('You searched for "' + searchedWord + '"');
 }
 
 
