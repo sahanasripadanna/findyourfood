5634e74f3b1fbee7eee2d5bf8b47a2fd6939226c
Asha Kar <Asha@Ashas-MacBook-Pro.local>
2018-05-04 19:42:19 -0700

    graphics

--- FinalProject/scripts.js
+++ FinalProject/scripts.js
@@ -1,12 +1,12 @@
 //function called if button is clicked
 function searched(){
-
+  document.getElementById("query-1").innerHTML = "";
   //points a reference to the element in the search
   var searchedWord = document.getElementById("searchTerm").value;
   document.getElementById("searchTerm").value ="";
   newElement("p", "You searched for '" + searchedWord +"'", 'result');
 
-  document.getElementById("query-1").innerHTML = "";
+  
  
   //processing the API through a request
     let request = new XMLHttpRequest();
@@ -17,7 +17,6 @@ function searched(){
 
     //event listener
     request.onreadystatechange = function() {
-      document.getElementById("query-1").innerHTML = "";
       //when the API server is ready, receive and add the elements to the page
       if (this.readyState === 4 && this.status === 200) {
         let response = JSON.parse(this.responseText);
@@ -26,7 +25,8 @@ function searched(){
           //alert(JSON.stringify(response.list.item[i].name));
           newElement("p", JSON.stringify(response.list.item[i].name),'query'+i);
           var currElement = document.getElementById('query'+i);
-          currElement.style.height = "200px";
+          currElement.style.height = "175px";
+          currElement.style.paddingTop = "12px";
           currElement.style.backgroundColor = "#d41274";
           currElement.style.float = "left";
           currElement.style.width = "49%";
@@ -49,11 +49,11 @@ function searched(){
 }
 
 //creates an element
-newElement = function(element, elText, thisname){
+newElement = function(element, elText,thisname){
   var newEl = document.createElement(element);   
   var newContent = document.createTextNode(elText); 
   newEl.appendChild(newContent);
-  var searches = document.getElementById('searches');
+  var searches = document.getElementById('query-1');
   searches.appendChild(newEl);
   newEl.id = thisname;
 }
