08b13d5fbd31f16b90d4314cc6021a382a715cf3
Asha Kar <Asha@Ashas-MacBook-Pro.local>
2018-05-24 15:34:34 -0700

    generic case done

--- FinalProject/scripts.js
+++ FinalProject/scripts.js
@@ -1,12 +1,8 @@
-var searchTerm;
 //function called if button is clicked
 function searched(){
   document.getElementById("query-1").innerHTML = "";
-
   //points a reference to the element in the search
   var searchedWord = document.getElementById("searchTerm").value;
-  searchTerm = searchedWord;
-  //if (!((searchedWord.equals("")) || (searchedWord.equals(" ")))){
     document.getElementById("searchTerm").value ="";
     newElement("p", "You searched for '" + searchedWord +"'", 'result', 'query-1');
 
@@ -14,7 +10,6 @@ function searched(){
   getResults("Fast+Foods", searchedWord, 0);
   getResults("Restaurant+Foods", searchedWord, 1000);
   }
-//}
 
 //creates an element
 //@param: element - type of element
@@ -35,9 +30,7 @@ newElement = function(element, elText,thisname, parentElement){
 
 //gathers the data from the api and adds it to the page
 getResults = function(foodType, searchedFood, queryStart){
- 
     let j = queryStart;
-    var count = 0;
     let request = new XMLHttpRequest();
       let url = "https://api.nal.usda.gov/ndb/search/?format=json";
       url+=("&q="+searchedFood);
@@ -67,55 +60,78 @@ getResults = function(foodType, searchedFood, queryStart){
               currElement.style.marginLeft = "10px";
             }
           }
-        
         }
-
       }
       //sends and creates the request
       request.open("GET", url, true);
       request.send();
-      
-      
   }
 
-
-
-noresults = function(){
-  alert("No results for this keyword");
-}
-
-//temporary methods for button click and response
+//opens the popup page if button is clicked
 dialog = function(x){
   var searchKey = document.getElementById('query' + x + '');
   var term = searchKey.innerText;
   var modal = document.getElementById('popUp');
   modal.style.display = "block";
-
-  postToPopUp(term);
+  var fixed = isolateName(term);
+  document.getElementById("innerText").innerText = term.substring(1, term.length - 2);
+  postToPopUp(fixed);
 } 
-//popup page
+
+//closes the popup page if 'x' is click
 function exit(){
-  
   var modal = document.getElementById('popUp');
   modal.style.display = "none";
 }
 
+
+//takes data from the API and adds it to the popup page
 function postToPopUp(keyWord){
+  let bodyText = document.getElementById("innerText");
+  let headBanner = document.getElementById('banner');
+  headBanner.innerText = keyWord;
+  if(keyWord.search("School Lunch") !== -1){
+    //adds default image and text for generic school lunch query
+    bodyText.innerHTML+= '<img src = "schoolLunch.png" alt = "school Lunch" style = "width : 50%; height : 50;">';
+
+  }else if((keyWord.search("Fast foods")!== -1) || (keyWord.search("Fast Food") !== -1)){
+    bodyText.innerHTML+= '<img src = "fastfood.png" alt = "fast food" style = "width : 30%; height : 30;">';
+  }else{
+  let placeId = "";
   let request = new XMLHttpRequest();
-  let url = "https://api.foursquare.com/v2/venues/search/?intent=global&query=";
+  let url = "https://api.foursquare.com/v2/venues/search/?near=Los+Angeles&query=";
   url+=keyWord;
   url+="&limit=1&client_id=NBVZZZSD5QBEA2SWONO22JHPQ3YUDJAHT3N4U4JYUSDSP0D3&client_secret=KP5HBRJX2Z3R3FJFJSMFT0SEGBN4TFRCZETYLKANOL5UMLCF&v=20180522";
   request.onreadystatechange = function() {
     //when the API server is ready, receive and add the elements to the page
     if (request.readyState === 4 && request.status === 200) {
       let response = JSON.parse(request.responseText);
-      let placeId = JSON.stringify(response.response.venues[0].id);
+      placeId = JSON.stringify(response.response.venues[0].id);
       alert(placeId);
     }
   }
       request.open("GET", url, true);
       request.send();
-   
-  
-  
+      let name = placeId.substring(1, placeId.length-2);
+  let getInfo = new XMLHttpRequest();
+  let link = "https://api.foursquare.com/v2/venues/";
+  let popUpText= document.getElementById('innerText');
+  link+=name;
+  link+="?client_id=NBVZZZSD5QBEA2SWONO22JHPQ3YUDJAHT3N4U4JYUSDSP0D3&client_secret=KP5HBRJX2Z3R3FJFJSMFT0SEGBN4TFRCZETYLKANOL5UMLCF&v=20180522";
+  getInfo.onreadystatechange = function(){
+    //when the API server is ready, receive and add the elements to the page
+    if(getInfo.readyState === 4 && getInfo.status === 200){
+      let getInfo = JSON.parse(request.responseText);
+      popText.innerText+=  JSON.stringify(getInfo);
+      
+    }
+  }
+}
+}
+
+//isolates only the store brand name, not the food and the place - removes the " " and ,
+function isolateName(keyWord){
+let comma = keyWord.indexOf(",");
+let newWord = keyWord.substring(1, comma);
+return newWord;
 }
